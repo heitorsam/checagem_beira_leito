de9769f879e3d1fe59c637319d71a0520d1445a0
santacasasjc <scalabrinih@gmail.com>
2022-02-24 09:06:55 -0300

    Melhorias

--- conexao.php
+++ conexao.php
@@ -22,7 +22,7 @@ $dbstr1 ="(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST =10.200.0.211)(PORT = 1
 (CONNECT_DATA = (SERVICE_NAME = prdmv)))";
 
 //Criar a conexao ORACLE
-if(!@($conn_ora = oci_connect('checkbeiraleito','tbpkrag#2021',$dbstr1,'AL32UTF8'))){
+if(!@($conn_ora = oci_connect('checkbeiraleito','beira_leito_stcasa_0849',$dbstr1,'AL32UTF8'))){
 	echo "Conexão falhou!";	
 } else { 
 	//echo "Conexão OK!";	
